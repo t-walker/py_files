48e0580dffa096d3012af3cece17fe19ee746e38
t-walker <tyler.p.walker@wsu.edu>
2015-02-12 21:57:11 +0000

    Initializing files

---
+++ filecopy.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+import os, sys, platform #Operating System Info
+import shutil #File Manipulation Info
+
+#Define Global Scope 
+SOURCE = ""
+DESTIN = ""
+
+def affirmative(answer):
+  if answer.lower() in ("y", "yes"):
+    return True
+  else:
+    return False
+
+def editConfig():
+  config = open(".config", "w")
+  source = input("[?] What is your source directory? ")
+  destin = input("[?] What is your destination directory? ")
+  print("SOURCE="+source, file=config)
+  print("DESTIN="+destin, file=config)
+
+def parseConfig():
+  config = open(".config", "r")
+  config = config.readlines() 
+  global SOURCE 
+  SOURCE = config[0].split("=")[1].strip()
+  global DESTIN
+  DESTIN = config[1].split("=")[1].strip()
+
+def main():
+  print("[!] You are currently working in:" + os.getcwd())
+  
+  answer = input("[?] Would you like to backup this directory? (Y/N) ")
+
+  if affirmative(answer):
+    parseConfig()
+    if os.path.exists(DESTIN):
+      shutil.rmtree(DESTIN)
+    shutil.copytree(SOURCE, DESTIN)
+  else:
+    print("[X] Function Terminating.")
+  return
+
+if __name__ == "__main__":
+  main() 
